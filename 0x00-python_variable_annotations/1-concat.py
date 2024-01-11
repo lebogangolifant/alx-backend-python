f078f46bf2572b3ead4151d2d98a658372c1190a
Lebogang <lebogangodeveloper@gmail.com>
2024-01-11 09:58:58 +0000

    Add type-annotated 'concat' function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function 'concat' that concatenates two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+    """
+    return str1 + str2
