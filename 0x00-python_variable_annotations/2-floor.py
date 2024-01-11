3ae9bf7f61cc739b3aab173ab0078b48bef6e273
Lebogang <lebogangodeveloper@gmail.com>
2024-01-11 10:04:24 +0000

    Add type-annotated 'floor' function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function 'floor' that returns the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float.
+    """
+    return math.floor(n)
