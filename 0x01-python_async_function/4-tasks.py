987a2207f0730a097d5658564d95d30676e58762
Lebogang <lebogangodeveloper@gmail.com>
2024-01-15 09:14:04 +0000

    Add task_wait_n function, which creates a list of asyncio

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Creating asyncio.Tasks using regular function syntax.
+"""
+
+import asyncio
+from typing import List
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    An asynchronous coroutine that simulates waiting for a random delay.
+    """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Creates an asyncio.Task for the wait_random function,
+    with the given max_delay.
+    """
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(wait_random(max_delay))
+    return task
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Creates a list of asyncio.Tasks for the wait_random function,
+    with the given max_delay.
+    """
+    loop = asyncio.get_event_loop()
+    tasks = [loop.create_task(wait_random(max_delay)) for _ in range(n)]
+    return await asyncio.gather(*tasks)
