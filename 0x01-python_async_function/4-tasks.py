3c2ca4bea02fa362fe852dcee99dbf08ec926306
Lebogang <lebogangodeveloper@gmail.com>
2024-01-15 11:56:10 +0000

    Refactor task_wait_n for improved readability and correctness

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,15 +5,15 @@ Creating asyncio.Tasks using regular function syntax.
 
 import asyncio
 from typing import List
-from random import uniform
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Creates a list of asyncio.Tasks for the wait_random function,
+    Creates a list of asyncio. Tasks for the wait_random function,
     with the given max_delay.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    delay = await asyncio.gather(*tasks)
+    return delay
