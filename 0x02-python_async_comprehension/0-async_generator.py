773aeea6e46117438a34187f639979649570b5c0
Lebogang <lebogangodeveloper@gmail.com>
2024-01-16 11:19:35 +0000

    Update the return type hint
    - to Generator[float, None, None] a regular generator that produces,
    floats and does not return any final result (None).

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,13 +5,13 @@ Async generator module
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
-    Coroutine that yields 10 random numbers between 0 and 10
+    Asynchronously generates 10 random floats with a 1-second delay
     """
     for _ in range(10):
+        yield random.random()
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
