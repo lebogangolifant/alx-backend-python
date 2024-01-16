0ad02d1fc37d0580fc0caeae05e3670591b58dc5
Lebogang <lebogangodeveloper@gmail.com>
2024-01-16 09:42:18 +0000

    Async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Async generator module
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Coroutine that yields 10 random numbers between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
