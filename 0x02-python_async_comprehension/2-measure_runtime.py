2f5c346f8ca48a2b78262943fb6ba051860f054b
Lebogang <lebogangodeveloper@gmail.com>
2024-01-16 12:40:04 +0000

    Update module for measuring execution time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,7 +4,7 @@ Measure Runtime module
 """
 
 import asyncio
-import timeit
+import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -14,13 +14,13 @@ async def measure_runtime() -> float:
     Measures the total runtime of executing async_comprehension
     four times in parallel.
     """
-    start_time = timeit.default_timer()
+    start_time = time.time()
 
     await asyncio.gather(
         *[async_comprehension() for _ in range(4)]
     )
 
-    end_time = timeit.default_timer()
+    end_time = time.time()
     total_runtime = end_time - start_time
 
     return total_runtime
