6e6199d333e1c85a40f965239c283cdeab2c2c0d
Lebogang <lebogangodeveloper@gmail.com>
2024-01-17 10:47:25 +0000

    Modify function structure

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,6 +4,7 @@ Measure Runtime module
 """
 
 import asyncio
+import random
 import timeit
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -14,19 +15,15 @@ async def measure_runtime() -> float:
     Measures the total runtime of executing async_comprehension
     four times in parallel.
     """
-    start_time = timeit.default_timer()
 
+    start_time = timeit.default_timer()
     await asyncio.gather(
-       *[
-             async_comprehension(),
-             async_comprehension(),
-             async_comprehension(),
-             async_comprehension()
-        ]
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
     )
 
     end_time = timeit.default_timer()
 
-    total_runtime = end_time - start_time
-
-    return total_runtime
+    return end_time - start_time
