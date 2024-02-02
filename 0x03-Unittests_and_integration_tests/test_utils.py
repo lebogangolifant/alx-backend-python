15580eb2ca24bbb24503451245e8e641899e3c38
Lebogang <lebogangodeveloper@gmail.com>
2024-02-02 08:59:13 +0000

    Add unit test for utils.get_json with mock.patch
    Implemented TestGetJson.test_get_json using unittest.mock.patch,
    to simulate HTTP calls and verify that utils.get_json returns the,
    expected result for different input scenarios.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,8 @@ Unit tests module for utils.access_nested_map.
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -35,3 +37,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(expected_exc, msg=expected_msg):
             access_nested_map(nested, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Class contains unit tests for utils.get_json.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Ensure get_json returns the expected result using unittest.mock.patch.
+        """
+        mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = test_payload
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
