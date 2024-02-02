cf2e9bd2f6161cd01b821c27f37c00d36fa45e5f
Lebogang <lebogangodeveloper@gmail.com>
2024-02-02 10:36:16 +0000

    Add unit test for the org method in GithubOrgClient class.
    - The test ensures that the org method returns the correct result,
    by patching the get_json method of the APIClient.
    - The test is parameterized with different organization names,
    and the patch prevents actual HTTP calls, allowing controlled testing.

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module contains unit tests for client.GithubOrgClient.
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Class contains unit tests for client.GithubOrgClient.
+    """
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json', autospec=True)
+    def test_org(self, org_name, mock_get_json):
+        """
+        Method to test org of GithubOrgClient
+        """
+        expected_result = {org_name: True}
+        mock_get_json.return_value = expected_result
+        github_org_client = GithubOrgClient(org_name)
+
+        result = github_org_client.org
+
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(result, expected_result)
